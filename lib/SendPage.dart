028cf125300af65c09ef7557f95d87db164c6790
CoreNion <coreinion@gmail.com>
2022-01-14 15:56:43 +0900

    [SendPage #5] file_pickerを導入、ファイル選択ボタンを実装

--- lib/SendPage.dart
+++ lib/SendPage.dart
@@ -1,3 +1,4 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
 class SendPage extends StatefulWidget {
@@ -8,12 +9,40 @@ class SendPage extends StatefulWidget {
 }
 
 class _SendPageState extends State<SendPage> {
+  var fileDataText = "<Result>";
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          "Send Page",
+    return Scaffold(
+      body: Container(
+        margin: const EdgeInsets.all(10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            // ファイル選択ボタン
+            SizedBox(
+              // width最大化
+              width: double.infinity,
+              height: 40,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.blue,
+                  onPrimary: Colors.white,
+                ),
+                child: const Text("Select file..."),
+                onPressed: () async {
+                  var file = await FilePicker.platform.pickFiles();
+                  setState(() {
+                    fileDataText = file.toString();
+                  });
+                },
+              ),
+            ),
+            Text(
+              fileDataText,
+              style: const TextStyle(),
+            ),
+          ],
         ),
       ),
     );
