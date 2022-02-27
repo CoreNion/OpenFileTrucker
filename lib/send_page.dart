9068b5b8656724e574219a808c088235e5a003a4
CoreNion <coreinion@gmail.com>
2022-02-27 16:46:45 +0900

    [send #5] 送信ページのファイル選択部のUIを改良
    レイアウト変更により空いたスペースを活用、将来的に複数ファイルの送信を見越したUIに
    ドラック&ドロップは現時点ではまだ未実装

--- lib/send_page.dart
+++ lib/send_page.dart
@@ -1,9 +1,11 @@
 import 'dart:io';
 
 import 'package:file_picker/file_picker.dart';
+import 'package:file_sizes/file_sizes.dart';
 import 'package:flutter/material.dart';
 import 'package:open_file_trucker/send.dart';
 import 'package:qr_flutter/qr_flutter.dart';
+import 'package:dotted_border/dotted_border.dart';
 
 class SendPage extends StatefulWidget {
   const SendPage({Key? key}) : super(key: key);
@@ -19,7 +21,12 @@ class _SendPageState extends State<SendPage>
 
   late bool isSmallUI;
   late FilePickerResult? selectedFile;
-  String fileDataText = "";
+  static Text firstFileButtonText = const Text(
+    "ファイルを選択\nまたはドラック&ドロップ",
+    style: TextStyle(fontSize: 30, color: Colors.blue),
+  );
+  Text selectFileButtonText = firstFileButtonText;
+  late String fileName;
   String serverStatus = "";
   String ipText = "";
   String keyText = "";
@@ -75,97 +82,115 @@ class _SendPageState extends State<SendPage>
   /// 小さな画面やウィンドウに最適化された送信ページのUI
   Scaffold smallUI() {
     return Scaffold(
-        body: Column(children: <Widget>[
-          selectFileArea(),
-          senderInfoArea(),
-        ]),
-        floatingActionButton: stopServerButton);
+        body: selectFileArea(), floatingActionButton: stopServerButton);
   }
 
   /// ファイルを選択する部分のUI
   Container selectFileArea() {
     return Container(
       margin: const EdgeInsets.all(10),
-      child: Column(children: <Widget>[
-        SizedBox(
-          width: double.infinity,
-          height: 40,
-          child: ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              primary: Colors.blue,
-              onPrimary: Colors.white,
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Expanded(
+              // TO DO ドラック&ドロップの実装
+              flex: 7,
+              child: Container(
+                  width: double.infinity,
+                  margin: const EdgeInsets.only(bottom: 20),
+                  child: DottedBorder(
+                    color: Colors.blueAccent,
+                    strokeWidth: 3,
+                    dashPattern: const [30, 5],
+                    child: SizedBox(
+                      width: double.infinity,
+                      height: double.infinity,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            primary: Colors.transparent,
+                            elevation: 0,
+                            shadowColor: Colors.black38),
+                        child: selectFileButtonText,
+                        onPressed: () async {
+                          var _selectedFile =
+                              await FilePicker.platform.pickFiles();
+                          if (!(_selectedFile == null)) {
+                            selectedFile = _selectedFile;
+                            fileName = _selectedFile.names[0]!;
+                            setState(() {
+                              selectFileButtonText = Text(
+                                "選択されたファイル:\n" +
+                                    fileName +
+                                    " " +
+                                    FileSize.getSize(
+                                        _selectedFile.files[0].size),
+                                style: const TextStyle(
+                                    color: Colors.blue, fontSize: 20),
+                              );
+                            });
+                          } else {
+                            setState(() {
+                              selectFileButtonText = firstFileButtonText;
+                              selectedFile = null;
+                            });
+                          }
+                        },
+                      ),
+                    ),
+                  )),
             ),
-            child: const Text("Select file..."),
-            onPressed: () async {
-              var _selectedFile = await FilePicker.platform.pickFiles();
-              if (!(_selectedFile == null)) {
-                selectedFile = _selectedFile;
-                setState(() {
-                  fileDataText = selectedFile.toString();
-                });
-              } else {
-                setState(() {
-                  fileDataText = "File not selected.";
-                  selectedFile = null;
-                });
-              }
-            },
-          ),
-        ),
-        Text(
-          fileDataText,
-          style: const TextStyle(),
-        ),
-        SizedBox(
-          width: double.infinity,
-          height: 40,
-          child: ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              primary: Colors.blueGrey,
-              onPrimary: Colors.white,
-            ),
-            child: const Text("Send"),
-            onPressed: () {
-              // ファイル選択時のみ実行
-              if (selectedFile != null) {
-                SendFiles.selectNetwork(context).then((ip) {
-                  if (!(ip == null)) {
-                    var file = File(selectedFile!.files.single.path!);
-                    SendFiles.serverStart(ip, "no", file).then((qr) {
-                      qrCode = qr;
-                      serverStatus = "受信待機中です。";
-                      ipText = "ip: " + ip;
-                      stopServerButton = FloatingActionButton(
-                        onPressed: _stopShareProcess,
-                        tooltip: '共有を停止する',
-                        child: const Icon(Icons.stop),
-                      );
-                      if (isSmallUI) {
-                        Navigator.of(context).push(MaterialPageRoute(
+            Expanded(
+              flex: 1,
+              child: SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.blueGrey,
+                    onPrimary: Colors.white,
+                  ),
+                  child: const Text("Send"),
+                  onPressed: () {
+                    // ファイル選択時のみ実行
+                    if (selectedFile != null) {
+                      SendFiles.selectNetwork(context).then((ip) {
+                        if (!(ip == null)) {
+                          var file = File(selectedFile!.files.single.path!);
+                          SendFiles.serverStart(ip, "no", file).then((qr) {
+                            qrCode = qr;
+                            serverStatus = "受信待機中です。";
+                            ipText = "ip: " + ip;
+                            stopServerButton = FloatingActionButton(
+                              onPressed: _stopShareProcess,
+                              tooltip: '共有を停止する',
+                              child: const Icon(Icons.stop),
+                            );
+                            if (isSmallUI) {
+                              Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) =>
+                                    _pushQRPageForSmallScreen(context),
+                              ));
+                            } else {
+                              setState(() {});
+                            }
+                          });
+                        } else {
+                          showDialog(
+                              context: context,
+                              builder: (context) =>
+                                  _showSmallInfo(context, "ネットワークに接続してください。"));
+                        }
+                      });
+                    } else {
+                      showDialog(
+                          context: context,
                           builder: (context) =>
-                              _pushQRPageForSmallScreen(context),
-                        ));
-                      } else {
-                        setState(() {});
-                      }
-                    });
-                  } else {
-                    showDialog(
-                        context: context,
-                        builder: (context) =>
-                            _showSmallInfo(context, "ネットワークに接続してください。"));
-                  }
-                });
-              } else {
-                showDialog(
-                    context: context,
-                    builder: (context) =>
-                        _showSmallInfo(context, "ファイルを選択してください。"));
-              }
-            },
-          ),
-        ),
-      ]),
+                              _showSmallInfo(context, "ファイルを選択してください。"));
+                    }
+                  },
+                ),
+              ),
+            ),
+          ]),
     );
   }
 
@@ -227,10 +252,10 @@ class _SendPageState extends State<SendPage>
                 textAlign: TextAlign.start,
                 text: TextSpan(
                     text: "受信待機中です",
-                    style: TextStyle(fontSize: 22),
+                    style: const TextStyle(fontSize: 22),
                     children: <TextSpan>[
                       TextSpan(
-                        text: '\n' + "ファイル名(仮)",
+                        text: '\n' + fileName,
                         style: const TextStyle(
                           fontSize: 14,
                         ),
