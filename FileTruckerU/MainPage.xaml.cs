5dfa790b464a8fa5fc05e30797e9d756fea023b9
CoreNion <coreinion@gmail.com>
2023-12-30 15:19:57 +0900

    Create navigationView pages

--- FileTruckerU/MainPage.xaml.cs
+++ FileTruckerU/MainPage.xaml.cs
@@ -1,3 +1,5 @@
+using FileTruckerU.Pages;
+
 namespace FileTruckerU;
 
 public sealed partial class MainPage : Page
@@ -5,5 +7,27 @@ public sealed partial class MainPage : Page
     public MainPage()
     {
         this.InitializeComponent();
+    }
+
+    private void NavigationView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+    {
+        if (args.IsSettingsSelected)
+        {
+
+        }
+        else
+        {
+            NavigationViewItem item = (NavigationViewItem)args.SelectedItem;
+
+            switch (item.Tag)
+            {
+                case "Receive":
+                    ContentFrame.Navigate(typeof(Receive));
+                    break;
+                case "Send":
+                    ContentFrame.Navigate(typeof(Send));
+                    break;
+            }
+        }   
     }
 }
