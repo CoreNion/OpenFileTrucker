1ca56f0e4c6d94228dad22c4b6e60ee9af8b96a4
CoreNion <coreinion@gmail.com>
2024-06-25 15:31:27 +0900

    [Android] Kotlinに移行

--- android/app/src/main/java/com/corenion/filetrucker/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.corenion.filetrucker;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
---
+++ android/app/src/main/kotlin/com/corenion/filetrucker/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.corenion.filetrucker;
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
